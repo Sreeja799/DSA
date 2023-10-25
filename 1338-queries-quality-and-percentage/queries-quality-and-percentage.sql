eabfa1aa8e24898217673e7776198054c4d67fb4
Sreeja Devisetti <sreejadevisetti@gmail.com>
2023-10-25 11:59:41 +0530

    Time: 622 ms (91.75%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,3 +1,5 @@
-SELECT q.query_name, ROUND(AVG(rating / position), 2) quality, ROUND((SELECT COUNT(query_name) FROM queries WHERE rating < 3 AND q.query_name = query_name) * 100 / COUNT(q.query_name), 2) poor_query_percentage
-FROM queries q
-GROUP BY q.query_name;
+SELECT query_name, 
+ROUND(AVG(rating / position), 2) quality, 
+ROUND(AVG(rating < 3) * 100, 2) poor_query_percentage
+FROM queries 
+GROUP BY query_name;
