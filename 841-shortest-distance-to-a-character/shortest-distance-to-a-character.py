d6ed877a64548d7a800192e11f6200081674cd74
Sreeja Devisetti <sreejadevisetti@gmail.com>
2023-10-26 18:48:32 +0530

    Time: 225 ms (5.08%) | Memory: 16.5 MB (16.79%) - LeetSync

---
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.py
@@ -0,0 +1,19 @@
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        n = len(s)
+        ans = [0 for i in range(n)]
+
+        for i in range(n):
+            if s[i] != c:
+                temp1 = temp2 = float('inf')
+                for j in range(i):
+                    if s[j] == c:
+                        temp1 = (i-j)
+
+                for j in range(i+1, n):
+                    if s[j] == c:
+                        temp2 = (j-i)
+                        break
+                print(temp1, temp2)
+                ans[i] = min(temp1, temp2)
+        return ans        
