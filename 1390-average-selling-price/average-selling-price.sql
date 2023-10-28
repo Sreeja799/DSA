0eb40cd3352ee1e532630a680b8fce6bf74c556c
Sreeja Devisetti <sreejadevisetti@gmail.com>
2023-10-28 17:54:39 +0530

    Time: 1788 ms (24.08%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,5 @@
+SELECT p.product_id, IFNULL(ROUND(SUM(p.price * u.units)/SUM(u.units), 2), 0) average_price
+FROM prices p 
+LEFT JOIN unitssold u
+ON p.product_id = u.product_id AND u.purchase_date >= p.start_date AND u.purchase_date <= p.end_date
+GROUP BY p.product_id
