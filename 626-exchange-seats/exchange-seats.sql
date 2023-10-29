84e9408e8afcfec1b3d6293d8f397bf369fb9548
Sreeja Devisetti <sreejadevisetti@gmail.com>
2023-10-29 19:59:48 +0530

    Time: 711 ms (51.32%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,19 +1,4 @@
-SELECT s2.id, s1.student
-FROM seat s1, seat s2
-WHERE s1.id = s2.id-1
-AND s1.id % 2 != 0
-
-UNION
-
-SELECT s2.id, s1.student
-FROM seat s1, seat s2
-WHERE s1.id = s2.id+1
-AND s1.id % 2 = 0
-
-UNION
-
-SELECT id, student
-FROM seat 
-WHERE id % 2 = 1 AND id = (SELECT MAX(id) FROM seat)
-
+SELECT
+IF (id < (SELECT COUNT(*) FROM seat), IF(id % 2 = 0, id-1, id+1), IF(id % 2 = 0, id-1, id)) id, student
+FROM seat
 ORDER BY id
