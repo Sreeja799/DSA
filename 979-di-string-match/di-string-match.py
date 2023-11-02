7e8ea6a77b5e0319e65f74f9926df3408e84fadb
Sreeja Devisetti <sreejadevisetti@gmail.com>
2023-11-02 21:13:59 +0530

    Time: 44 ms (99.79%) | Memory: 17.5 MB (67.81%) - LeetSync

--- 979-di-string-match/di-string-match.py
+++ 979-di-string-match/di-string-match.py
@@ -1,16 +1,15 @@
 class Solution:
     def diStringMatch(self, s: str) -> List[int]:
-        n = len(s)
-        arr = [i for i in range(n+1)]
-        l = [0 for i in range(n+1)]
+        l = 0
+        r = len(s)
+        ans = []
 
-        for i in range(len(s)):
-            if s[i] == 'D':
-                l[i] = max(arr)
-                arr.remove(max(arr))
-        
-        for i in range(len(l)):
-            if l[i] == 0 and len(arr) > 0:
-                l[i] = arr[0]
-                arr.pop(0)
-        return l
+        for i in s:
+            if i == 'I':
+                ans.append(l)
+                l += 1
+            else:
+                ans.append(r)
+                r -= 1
+        ans.append(l)
+        return ans
